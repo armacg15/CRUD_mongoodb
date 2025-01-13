b5702a464d75b68f3c695459cd88b1b70606a616
armacg15 <103144689+armacg15@users.noreply.github.com>
2025-01-13 15:55:42 -0600

    alumnos.js

--- routes/alumnos.js
+++ routes/alumnos.js
@@ -3,12 +3,12 @@ const router = express.Router()
 
 const alumnoController = require('../controllers/alumnoContoller')
 
-//Mostrar todos los alumnos (GET)
+
 router.get('/', alumnoController.mostrar)
-//Crear alumno (POST)
+
 router.post('/crear', alumnoController.crear)
-//Editar alumno (POST)
+
 router.post('/editar', alumnoController.editar)
-//Borrar alumno (GET)
+
 router.get('/borrar/:id', alumnoController.borrar)
 module.exports = router
