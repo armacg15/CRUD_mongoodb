d26e838da9cba8ea0ae887f77032120bc2dbd055
armacg15 <103144689+armacg15@users.noreply.github.com>
2025-01-13 15:53:35 -0600

    alumnoContoller.js

--- controllers/alumnoContoller.js
+++ controllers/alumnoContoller.js
@@ -1,13 +1,13 @@
-const Alumno = require('../model/Alumno'); // Importa correctamente el modelo
+const Alumno = require('../model/Alumno'); 
 
 // Mostrar todos los alumnos
 module.exports.mostrar = (req, res) => {
     Alumno.find({})
         .then(alumnos => {
-            res.render('index', { alumnos }); // Renderiza la vista con los datos
+            res.render('index', { alumnos }); 
         })
         .catch(err => {
-            res.status(500).send(err.message); // Maneja errores
+            res.status(500).send(err.message); 
         });
 };
 
@@ -19,7 +19,7 @@ module.exports.crear = (req, res) => {
     });
 
     alumno.save()
-        .then(() => res.redirect('/')) // Redirige después de guardar
+        .then(() => res.redirect('/')) 
         .catch(() => {
             res.status(500).json({ message: 'Error al crear el Alumno' });
         });
@@ -30,7 +30,7 @@ module.exports.editar = (req, res) => {
     const { id_editar: id, nombre_editar: nombre, edad_editar: edad } = req.body;
 
     Alumno.findByIdAndUpdate(id, { nombre, edad })
-        .then(() => res.redirect('/')) // Redirige después de actualizar
+        .then(() => res.redirect('/')) 
         .catch(() => {
             res.status(500).json({ message: 'Error actualizando el Alumno' });
         });
@@ -41,7 +41,7 @@ module.exports.borrar = (req, res) => {
     const id = req.params.id;
 
     Alumno.findByIdAndDelete(id)
-        .then(() => res.redirect('/')) // Redirige después de eliminar
+        .then(() => res.redirect('/')) 
         .catch(() => {
             res.status(500).json({ message: 'Error eliminando el Alumno' });
         });
